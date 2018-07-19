8d0823c7e3ca08e487c1f91060893fb16be50f1e
changyu2 <lee930305@gmail.com>
2018-07-19 20:30:23 +0900

    Getting data from the GraphQL API #2

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,18 @@ import React from "react";
 import { Query } from "react-apollo";
 import { HOME_PAGE } from "./queries";
 
-const Home = () => <Query query={HOME_PAGE}>{}</Query>;
+const Home = () => (
+  <Query query={HOME_PAGE}>
+    {({ loading, data, error }) => {
+      if (loading) return "loading";
+      if (error) return "error has occured!";
+      return data.movies.map(movie => (
+        <h2 key={movie.id}>
+          {movie.title} / {movie.rating}
+        </h2>
+      ));
+    }}
+  </Query>
+);
 
 export default Home;
